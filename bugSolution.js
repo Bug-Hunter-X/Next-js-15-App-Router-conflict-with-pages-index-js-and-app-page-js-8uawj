15f65fd4ce3d87a674f6d104f239063afff33f85
BugHunter-X <askzenai@gmail.com>
2025-01-30 13:32:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,11 @@
+```javascript
+// Remove pages/index.js
+
+// Or, modify app/page.js to handle routing differently if needed:
+// app/page.js
+export default function App() {
+  return (
+    <div>App component</div>
+  );
+}
+```
